1abce66ebd1cbfb400538d91aae881d00b8ed3e6
Arjen017 <arjen.esmenda@gmail.com>
2024-10-31 15:46:44 +0800

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,17 @@ function hideDropdown() {
 // Delay in milliseconds (5 seconds)
     };
 });
+
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
+
