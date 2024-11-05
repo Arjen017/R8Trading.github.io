f2e16946a16d7694c8299a2fbd87c684aa485595
Arjen017 <arjen.esmenda@gmail.com>
2024-11-05 17:10:47 +0800

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,26 +10,8 @@ document.addEventListener("DOMContentLoaded", function() {
         if (counter >= images.length) {
             counter = 0;
         }
-        carouselSlide.style.transform = `translateX(${-counter * 350}px)`;
+        carouselSlide.style.transform = `translateX(${-counter * 640}px)`;
     }
 
-    setInterval(carousel, 3000); // Change slide every 3 seconds
-
-    // Function to show dropdown on hover and keep it visible for 5 seconds
-let dropdownTimeout;
-
-function showDropdown() {
-    clearTimeout(dropdownTimeout);
-    document.getElementById("batteryDropdown").style.display = "block";
-}
-
-function hideDropdown() {
-    dropdownTimeout = setTimeout(function() {
-        document.getElementById("batteryDropdown").style.display = "none";
-    }, 5000);
-}
-// Delay in milliseconds (5 seconds)
-    };
+    setInterval(carousel, 3000);
 });
-
-
