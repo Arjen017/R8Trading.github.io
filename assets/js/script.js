a422909401e3f7d442786f3be5631257ab6cdb45
Arjen017 <arjen.esmenda@gmail.com>
2024-11-06 17:09:08 +0800

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+      const carouselSlide = document.querySelector(".carousel-slide");
+    const images = document.querySelectorAll(".carousel-slide img");
+    let counter = 0;
+    const slideWidth = images[0].clientWidth;
+
+    function showSlide() {
+        carouselSlide.style.transform = `translateX(${-counter * slideWidth}px)`;
+    }
+
+    function nextSlide() {
+        counter = (counter + 1) % images.length;
+        showSlide();
+    }
+
+    function prevSlide() {
+        counter = (counter - 1 + images.length) % images.length;
+        showSlide();
+    }
+
+    setInterval(nextSlide, 3000);
+
+    window.addEventListener("resize", () => {
+        showSlide();
+    });
+
     // Initialize Slick carousel with fade effect
     $('.carousel').slick({
         dots: true,
