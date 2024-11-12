9cd8966b6ab49742f58573e1e0b8f38428d0a06f
Arjen017 <arjen.esmenda@gmail.com>
2024-11-12 10:30:34 +0800

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Get elements
     const dropdown = document.querySelector('.dropdown');
     const batteryDropdown = document.getElementById('batteryDropdown');
     const navigation = document.querySelector('.navigation');
 
+    // Show dropdown menu when hovering over navigation
     navigation.addEventListener('mouseenter', function() {
         batteryDropdown.classList.add('show');
     });
 
+    // Hide dropdown menu when leaving navigation
     navigation.addEventListener('mouseleave', function() {
         batteryDropdown.classList.remove('show');
     });
@@ -17,22 +20,27 @@ document.addEventListener("DOMContentLoaded", function() {
     let counter = 0;
     const slideWidth = images[0].clientWidth;
 
+    // Function to show current slide
     function showSlide() {
         carouselSlide.style.transform = `translateX(${-counter * slideWidth}px)`;
     }
 
+    // Move to the next slide
     function nextSlide() {
         counter = (counter + 1) % images.length;
         showSlide();
     }
 
+    // Move to the previous slide
     function prevSlide() {
         counter = (counter - 1 + images.length) % images.length;
         showSlide();
     }
 
+    // Auto-slide every 3 seconds
     setInterval(nextSlide, 3000);
 
+    // Adjust the slide on window resize
     window.addEventListener("resize", () => {
         showSlide();
     });
