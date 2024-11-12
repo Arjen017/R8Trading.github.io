44530751ccf97f7bbbecbebafc723a1a14be4b72
Arjen017 <arjen.esmenda@gmail.com>
2024-11-12 10:14:55 +0800

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const dropdown = document.querySelector('.dropdown');
     const batteryDropdown = document.getElementById('batteryDropdown');
-    let dropdownOpen = false;
+    const navigation = document.querySelector('.navigation');
 
-    // Show dropdown menu on hover of the dropdown
-    dropdown.addEventListener('mouseenter', function() {
+    navigation.addEventListener('mouseenter', function() {
         batteryDropdown.classList.add('show');
-        dropdownOpen = true;
     });
 
-    // Hide dropdown menu only when the mouse leaves both the dropdown and the menu itself
-    dropdown.addEventListener('mouseleave', function() {
-        setTimeout(() => {
-            if (!dropdown.matches(':hover') && !batteryDropdown.matches(':hover')) {
-                batteryDropdown.classList.remove('show');
-                dropdownOpen = false;
-            }
-        }, 100); // Add a slight delay to ensure the menu doesn't disappear too quickly
-    });
-
-    batteryDropdown.addEventListener('mouseleave', function() {
-        setTimeout(() => {
-            if (!dropdown.matches(':hover') && !batteryDropdown.matches(':hover')) {
-                batteryDropdown.classList.remove('show');
-                dropdownOpen = false;
-            }
-        }, 100);
+    navigation.addEventListener('mouseleave', function() {
+        batteryDropdown.classList.remove('show');
     });
 
     // Carousel functionality
