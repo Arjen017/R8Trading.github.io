045cbbcb34f57f513524ba73306ad3d8b2bcf1bf
Arjen017 <arjen.esmenda@gmail.com>
2024-11-15 16:57:45 +0800

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const mapLink = document.getElementById("mapLink");
     const carouselSection = document.getElementById("carouselSection");
     const mapSection = document.getElementById("mapSection");
+
+    homeLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("Home link clicked");
+    mapSection.classList.add("hidden");
+    carouselSection.classList.remove("hidden");
+});
     
     mapLink.addEventListener("click", function(event) {
     event.preventDefault();
